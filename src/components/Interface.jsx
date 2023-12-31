402c93d981e6a227066b4a313842233d95552010
Patrick Sardinha <patricksardinha.pro@gmail.com>
2023-12-31 16:30:17 +0100

    add motion framer & update interface components with section

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,24 +1,56 @@
+import { motion } from "framer-motion";
+
 const Section = (props) => {
     const { children } = props;
 
     return (
-        <section className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}>
+        <motion.section
+            initial={{
+                opacity: 0,
+                y: 50
+            }}
+            whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                    duration: 1,
+                    delay: 0.6
+                }
+            }}
+            className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}
+        >
             {children}
-        </section>
+        </motion.section>
     );
 };
 
 const HomeSection = () => {
     return (
         <Section>
-            <h1 className="text-6xl font-extrabold leading-snug">
-                Hello, My name is <br/>
-                <span className="bg-white px-1 italic">Patrick Sardinha</span>
-            </h1>
-            <p className="text-lg text-gray-600 mt-4">
-                I am a Fullstack Developper and I love 3D Modeling. <br/>
-                Welcome to my portfolio.
-            </p>
+            <div className="w-1/2">
+                <h1 className="text-6xl font-extrabold leading-snug">
+                    Hello, My name is <br/>
+                    <span className="bg-white px-1 italic">Patrick Sardinha</span>
+                </h1>
+                <motion.p
+                    initial={{
+                        opacity: 0,
+                        y: 25
+                    }}
+                    whileInView={{
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            duration: 1,
+                            delay: 1.5
+                        }
+                    }}
+                    className="text-lg text-gray-600 mt-4"
+                >
+                    I am a Fullstack Developper and I love 3D Modeling. <br/>
+                    Welcome to my portfolio.
+                </motion.p>
+            </div>
         </Section>
     );
 };
@@ -26,7 +58,22 @@ const HomeSection = () => {
 const AboutSection = () => {
     return (
         <Section>
-            <h1>Home</h1>
+            <div className="w-1/3">
+                <h3 className="text-3xl font-extrabold leading-snug">Learn more about me.</h3>
+                <p className="text-lg text-gray-600 mt-4">
+                    In high school I did a Scientific Baccalaureate with the specialty "Computer Science and Digital Sciences" thanks to which today I have a real 
+                    interest in computer development. <br/>
+
+                    I thus continued my studies by doing a Bachelor and a Master in Computer Science at the University of Geneva. It was there that I discovered and learned 
+                    a lot about all areas of IT through specific courses. <br/>
+
+                    After university, to specialize in a field, I did online Full Stack Web development formation provided by the Hong Kong University of Science & Technology. 
+                    Being also passionate about real-time applications and 3D modeling, I decided after my formation to follow an Unity and an Unreal Engine formation but also
+                    to learn how to work with Blender to use and create 3D assets. <br/>
+
+                    Now, my goal is to improve and put my knowledge into practice through many projects.
+                </p>
+            </div>
         </Section>
     );
 };
@@ -34,7 +81,7 @@ const AboutSection = () => {
 const ProjectsSection = () => {
     return (
         <Section>
-            <h1>Home</h1>
+            <h3 className="text-3xl font-extrabold leading-snug">Projects</h3>
         </Section>
     );
 };
@@ -42,7 +89,7 @@ const ProjectsSection = () => {
 const ExperiencesSection = () => {
     return (
         <Section>
-            <h1>Home</h1>
+            <h3 className="text-3xl font-extrabold leading-snug">Experiences</h3>
         </Section>
     );
 };
@@ -51,7 +98,7 @@ const ExperiencesSection = () => {
 const ContactSection = () => {
     return (
         <Section>
-            <h1>Home</h1>
+            <h3 className="text-3xl font-extrabold leading-snug">Contact</h3>
         </Section>
     );
 };
@@ -59,14 +106,12 @@ const ContactSection = () => {
 export const Interface = () => {
 
     return (
-        <div className="">
-            <div className="flex flex-col items-center w-screen">
-                <HomeSection/>
-                <AboutSection/>
-                <ProjectsSection/>
-                <ExperiencesSection/>
-                <ContactSection/>
-            </div>
+        <div className="flex flex-col items-center w-screen">
+            <HomeSection/>
+            <AboutSection/>
+            <ProjectsSection/>
+            <ExperiencesSection/>
+            <ContactSection/>
         </div>
     );
 };
